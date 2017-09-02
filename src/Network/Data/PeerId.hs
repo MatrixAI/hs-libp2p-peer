7876f1440f098da311cc3f13fc7b25ae95d1afab
Quoc-An Ho <quocanho29@gmail.com>
2017-09-02 17:55:14 +1000

    Add parsing, serialization and module description
    Provides parsing and serialization methods for various byte
    representations of a Peer ID.
    
    We use the MultihashDigest type from haskell-multihash for the
    underlying representation of a PeerId. This representation should make
    it easier to ensure that PeerIds are correctly constructed.
    
    Pretty printing of a Peer ID matches the reference implementation at
    libp2p/go-libp2p-peer, where the first "Qm" bytes of the multihash are
    filtered and six chars of the multihash are pinted.
    
    Also adds a basic module description.

--- src/Network/Data/PeerId.hs
+++ src/Network/Data/PeerId.hs
@@ -1,77 +1,101 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-|
+Module      : Network.Data.PeerId
+Description : Multihash public key identities for LibP2P
+License     : Apache-2.0
+Maintainer  : quoc.ho@matrix.ai
+Stability   : experimental
 
+Provides a PeerId type, which is a SHA256 multihash of an underlying public key.
+This is used as an identifier through the LibP2P system.
+At the moment, we exclusively use RSA public keys, pending changes to 
+the reference libp2p-crypto implementation to use a wrapped key type (multikey).
+-}
 module Network.Data.PeerId where
 
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as TEncoding
-import qualified Data.ByteString as BSStrict
-import qualified Data.ByteString.Lazy as BSLazy
-import qualified Data.ByteString.Char8 as BSStrictChar
-import qualified Data.Multihash.Base as MHB
-import qualified Data.Multihash.Digest as MHD
+import qualified Crypto.LibP2P.Serialize as Crypto
 
-import GHC.Generics (Generic)
-import Data.Typeable (Typeable)
-import Data.Data (Data)
-import Data.String (IsString, fromString)
+import qualified Data.Text               as T
+import qualified Data.Text.Encoding      as TE
+import qualified Data.ByteString         as BS
+import qualified Data.ByteString.Lazy    as BSL
+import qualified Data.ByteString.Char8   as BSChar
+import qualified Data.Multihash.Base     as MHB
+import qualified Data.Multihash.Digest   as MHD
 
---newtype PeerId = PeerId BSStrict.ByteString
-data PeerId = PeerId
-  {
-    --multihash of the peer id
-    peerHash :: MHD.MultihashDigest
-    --secretKey :: PrivKey a b
-    --publicKey :: PubKey a b
-  }
+newtype PeerId = PeerId { unPeerId :: MHD.MultihashDigest } 
   deriving (Show, Eq)
-
-instance IsString PeerId where
-  fromString s = either error id $ fromBytes $ BSStrictChar.pack s
-
--- Create a Peer ID from raw bytes representing the key's multihash
-fromBytes :: BSStrict.ByteString -> Either String PeerId
-fromBytes bs = fmap PeerId $ MHD.decode bs
-
--- Create a Peer ID from a base encoded bytestring representing a multihash
-fromBase :: MHB.BaseEncoding -> BSStrict.ByteString -> Either String PeerId
-fromBase base bs = (fmap BSLazy.toStrict $ MHB.decode base $ BSLazy.fromStrict bs) >>= fromBytes
-
--- Convenience methods for fromBase
-fromHex :: BSStrict.ByteString -> Either String PeerId
-fromHex = fromBase MHB.Base16
-
--- Convenience methods for fromBase
-fromB58 :: BSStrict.ByteString -> Either String PeerId
-fromB58 = fromBase MHB.Base58
-
--- Convenience methods for fromBase
-fromB64 :: BSStrict.ByteString -> Either String PeerId
-fromB64 = fromBase MHB.Base64
 
 prettyPrint :: PeerId -> T.Text
 prettyPrint peerId = T.concat
   [
     "<peer.ID ",
-    T.take maxChars $ filter $ TEncoding.decodeUtf8 
-    $ MHD.digest
-    $ peerHash peerId,
+    T.take maxChars 
+    $ filter 
+    $ TE.decodeUtf8 
+    $ BSL.toStrict
+    $ MHB.encode MHB.Base58
+    $ BSL.fromStrict
+    $ MHD.digest 
+    $ unPeerId peerId,
     ">"
   ]
   where
     maxChars = 6
     filter t = maybe t id $ T.stripPrefix "Qm" t
 
--- TODO: Until the issues with haskell-libp2p-crypto are resolved,
--- leave key resolution out of representations of a PeerId,
--- and only get PeerIds from fromBytes
--- keyToPeerId :: (PubKey a) => a -> PeerId
--- keyToPeerId pubkey = PeerId . MHD.digest $ MHD.encode SHA256 $ toBytes pubkey
+-- Create a Peer ID from raw bytes representing the key's multihash
+-- The multihash must have a SHA256 encoded digest, or we return an error
+fromBytes :: BS.ByteString -> Either String PeerId
+fromBytes bs = do
+  mhd <- MHD.decode bs
+  case MHD.algorithm mhd of
+       MHD.SHA256 -> Right (PeerId mhd)
+       _          -> Left "PeerId Multihashes need to be SHA256 encoded"
 
--- matchesPrivKey :: (PrivKey a b) => PeerId -> b -> Bool
--- matchesPrivKey peerId privKey = matchesPubKey peerId $ toPublic privKey
+-- Create a Peer ID from a base encoded bytestring representing a multihash
+fromBase :: MHB.BaseEncoding -> BS.ByteString -> Either String PeerId
+fromBase base bs = unBase bs >>= fromBytes
+  where
+    unBase bs = 
+      fmap BSL.toStrict 
+      $ MHB.decode base 
+      $ BSL.fromStrict bs
 
--- matchesPubKey :: (PrivKey a b) => PeerId -> a -> Bool
--- matchesPubKey peerId pubKey = peerId == keyToPeerId pubKey 
+-- Convenience methods for fromBase
+fromHex :: BS.ByteString -> Either String PeerId
+fromHex = fromBase MHB.Base16
+
+fromB58 :: BS.ByteString -> Either String PeerId
+fromB58 = fromBase MHB.Base58
+
+fromB64 :: BS.ByteString -> Either String PeerId
+fromB64 = fromBase MHB.Base64
+
+-- Return the byte representation of the Peer ID
+-- (i.e. the SHA256 multihash of an RSA key)
+toBytes :: PeerId -> BS.ByteString
+toBytes pid = unPeerId pid
+
+-- Return a base encoded byte representation of the Peer ID multhash digest
+toBase :: MHB.BaseEncoding -> PeerId -> BS.ByteString
+toBase base pid = MHB.encode base $ unPeerId pid
+
+-- Convenience methods for toBase
+toHex :: PeerId -> BS.ByteString
+toHex = toBase MHB.Base16
+
+toB58 :: PeerId -> BS.ByteString
+toB58 = toBase MHB.Base58
+
+toB64 :: PeerId -> BS.ByteString
+toB64 = toBase MHB.Base64
+
+-- Convert an RSA public key to a PeerID 
+rsaToPeerId :: RSA.PublicKey -> PeerId
+rsaToPeerId pk = PeerId mhd
+  where
+    Right mhd = MHD.decode digest
+    digest = 
+      BSL.toStrict 
+      $ MHD.encode MHD.SHA256 
+      $ Crypto.serialize pk
