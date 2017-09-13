6a8b40aa63a2fb25ecbd1edfbfb5fa3884fa4bdf
Quoc-An Ho <quocanho29@gmail.com>
2017-09-13 20:37:26 +1000

    Add quickcheck and unit testing and fixes
    Adds quickcheck and unit tests based on the reference tests at
    github.com/libp2p/go-libp2p-peer.
    
    Specifically, we add a unit test to check the peerId encoding against
    reference keys from the go-libp2p-peer and js-peer-id repos, and
    quickcheck tests that ensure a peerid is the same after encoding and
    decoding to the PeerId type.
    
    At the moment, we only do testing on RSA Keys. TODO: We need to implement a
    better test suite to test over multiple key types.
    
    Dependencies are added and updated as needed to support the changes.

--- src/Network/LibP2P/PeerId.hs
+++ src/Network/LibP2P/PeerId.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-|
 Module      : Network.LibP2P.PeerId
@@ -9,47 +9,52 @@ Stability   : experimental
 
 Provides a PeerId type, which is a SHA256 multihash of an underlying public key.
 This is used as an identifier through the LibP2P system.
-At the moment, we exclusively use RSA public keys, pending changes to 
+At the moment, we exclusively use RSA public keys, pending changes to
 the reference libp2p-crypto implementation to use a wrapped key type (multikey).
 -}
 module Network.LibP2P.PeerId where
 
+import qualified Crypto.LibP2P.PrivKey   as Crypto
 import qualified Crypto.LibP2P.Serialize as Crypto
 
-import qualified Data.Text               as T
-import qualified STMContainers.Set       as STMC
-import qualified Data.Text.Encoding      as TE
+import qualified Crypto.PubKey.RSA       as RSA
 import qualified Data.ByteString         as BS
 import qualified Data.ByteString.Lazy    as BSL
 import qualified Data.Multihash.Base     as MHB
 import qualified Data.Multihash.Digest   as MHD
-import qualified Crypto.PubKey.RSA       as RSA
+import qualified Data.Text               as T
+import qualified Data.Text.Encoding      as TE
+import qualified STMContainers.Set       as STMC
 
-import           GHC.Generics               (Generic)
+import           GHC.Generics            (Generic)
 
-newtype PeerId = PeerId { unPeerId :: MHD.MultihashDigest } 
+
+newtype PeerId = PeerId { unPeerId :: MHD.MultihashDigest }
   deriving (Show, Eq, Generic)
+
 
 -- Threadsafe collection of PeerIds
 type PeerSet = STMC.Set PeerId
+
 
 prettyPrint :: PeerId -> T.Text
 prettyPrint peerId = T.concat
   [
     "<peer.ID ",
-    T.take maxChars 
-    $ filter 
-    $ TE.decodeUtf8 
+    T.take maxChars
+    $ filter
+    $ TE.decodeUtf8
     $ BSL.toStrict
     $ MHB.encode MHB.Base58
     $ BSL.fromStrict
-    $ MHD.digest 
+    $ MHD.digest
     $ unPeerId peerId,
     ">"
   ]
   where
     maxChars = 6
     filter t = maybe t id $ T.stripPrefix "Qm" t
+
 
 -- Create a Peer ID from raw bytes representing the key's multihash
 -- The multihash must have a SHA256 encoded digest, or we return an error
@@ -60,61 +65,73 @@ fromBytes bs = do
        MHD.SHA256 -> Right (PeerId mhd)
        _          -> Left "PeerId Multihashes need to be SHA256 encoded"
 
+
 -- Create a Peer ID from a base encoded bytestring representing a multihash
 fromBase :: MHB.BaseEncoding -> BS.ByteString -> Either String PeerId
 fromBase base bs = unBase bs >>= fromBytes
   where
-    unBase bs = 
-      fmap BSL.toStrict 
-      $ MHB.decode base 
+    unBase bs =
+      fmap BSL.toStrict
+      $ MHB.decode base
       $ BSL.fromStrict bs
+
 
 -- Convenience methods for fromBase
 fromHex :: BS.ByteString -> Either String PeerId
 fromHex = fromBase MHB.Base16
 
+
 fromB58 :: BS.ByteString -> Either String PeerId
 fromB58 = fromBase MHB.Base58
+
 
 fromB64 :: BS.ByteString -> Either String PeerId
 fromB64 = fromBase MHB.Base64
 
--- Return the byte representation of the Peer ID
--- (i.e. the SHA256 multihash of an RSA key)
-toBytes :: PeerId -> BS.ByteString
-toBytes pid = MHD.digest $ unPeerId pid
 
--- Return a base encoded byte representation of the Peer ID multhash digest
-toBase :: MHB.BaseEncoding -> PeerId -> BS.ByteString
-toBase base pid = 
+-- Return the byte representation of the Peer ID
+-- (i.e. the SHA256 multihash of an RSA Public key)
+toBytes :: PeerId -> BS.ByteString
+toBytes pid =
   BSL.toStrict
-  $ MHB.encode base 
-  $ BSL.fromStrict
-  $ MHD.digest 
+  $ MHD.encodeMultihashDigest
   $ unPeerId pid
+
+
+-- Return a base encoded byte representation of the Peer ID multihash digest
+toBase :: MHB.BaseEncoding -> PeerId -> BS.ByteString
+toBase base pid =
+  BSL.toStrict
+  $ MHB.encode base
+  $ BSL.fromStrict
+  $ toBytes pid
+
 
 -- Convenience methods for toBase
 toHex :: PeerId -> BS.ByteString
 toHex = toBase MHB.Base16
 
+
 toB58 :: PeerId -> BS.ByteString
 toB58 = toBase MHB.Base58
+
 
 toB64 :: PeerId -> BS.ByteString
 toB64 = toBase MHB.Base64
 
--- Convert an RSA public key to a PeerID 
+
+-- Convert an RSA public key to a PeerID
 rsaToPeerId :: RSA.PublicKey -> PeerId
 rsaToPeerId pk = PeerId mhd
   where
-    Right mhd = MHD.decode digest
-    digest = 
-      BSL.toStrict 
-      $ MHD.encode MHD.SHA256 
+    mhd =
+      MHD.multihash MHD.SHA256
       $ Crypto.serialize pk
 
+
 matchesRSAPublicKey :: RSA.PublicKey -> PeerId -> Bool
-matchesRSAPublicKey pk pid = (rsaToPeerId pk) == pid 
+matchesRSAPublicKey pk pid = (rsaToPeerId pk) == pid
+
 
 matchesRSAPrivateKey :: RSA.PrivateKey -> PeerId -> Bool
 matchesRSAPrivateKey sk pid = (rsaToPeerId $ Crypto.toPublic sk) == pid
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,131 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+import Network.LibP2P.PeerId
+
+import qualified Crypto.LibP2P.Key          as Crypto
+import qualified Crypto.LibP2P.PubKey       as Crypto
+import qualified Crypto.LibP2P.PrivKey      as Crypto
+import qualified Crypto.LibP2P.Serialize    as Crypto
+import qualified Crypto.LibP2P.Parse        as Crypto
+import qualified Crypto.PubKey.RSA          as RSA
+
+import qualified Data.Aeson                 as Aeson
+import qualified Data.ByteArray             as BA
+import qualified Data.ByteString            as BS
+import qualified Data.ByteString.Lazy       as BSL
+import qualified Data.ByteString.Char8      as BSChar
+import qualified Data.ByteString.Lazy.Char8 as BSLChar
+import qualified Data.Multihash.Base        as MHB
+import qualified Data.Multihash.Digest      as MHD
+
+import qualified Test.Tasty                 as Tasty
+import qualified Test.Tasty.QuickCheck      as Tasty
+import qualified Test.Tasty.HUnit           as Tasty
+
+import qualified Test.QuickCheck            as QC
+import qualified Test.QuickCheck.Arbitrary  as QC
+import qualified Test.QuickCheck.Gen        as QC
+import qualified Test.QuickCheck.Monadic    as QC
+
+import           Crypto.Random.Types           (MonadRandom(..)) 
+import           Control.Monad                 (replicateM, liftM2) 
+import           Data.Word                     (Word8) 
+import           GHC.Generics                  (Generic)
+
+
+data RSAKeyPeerIdPair = RSAKeyPeerIdPair {
+    base58PeerId :: String, 
+    base64RSAPrivateKey :: String
+} deriving (Generic, Show)
+
+
+instance Aeson.FromJSON RSAKeyPeerIdPair
+instance Aeson.ToJSON RSAKeyPeerIdPair
+ 
+
+-- Adds the ability for quickcheck generators to supply random bytes
+instance MonadRandom QC.Gen where
+  getRandomBytes n = do
+    words <- replicateM n $ (QC.chooseAny :: QC.Gen Word8)
+    return $ BA.pack words
+
+
+instance QC.Arbitrary MHB.BaseEncoding where
+  arbitrary = QC.arbitraryBoundedEnum
+
+
+instance QC.Arbitrary RSA.PublicKey where
+  arbitrary = do
+    (pk, sk) <- RSA.generate 128 65537
+    return pk
+
+
+instance QC.Arbitrary RSA.PrivateKey where
+  arbitrary = do
+    (pk, sk) <- RSA.generate 128 65537
+    return sk
+
+
+prop_encodeEquals :: MHB.BaseEncoding -> RSA.PublicKey -> Bool
+prop_encodeEquals b pk = pid == pid2
+  where
+    pid = rsaToPeerId pk
+    Right pid2 = fromBase b $ toBase b pid
+
+
+prop_matchPublicKey :: RSA.PublicKey -> Bool
+prop_matchPublicKey pk = matchesRSAPublicKey pk pid
+  where
+    pid = rsaToPeerId pk
+
+
+prop_matchPrivateKey :: RSA.PrivateKey -> Bool
+prop_matchPrivateKey sk = matchesRSAPrivateKey sk pid
+  where
+    pid = rsaToPeerId $ Crypto.toPublic sk
+
+
+quickCheckTests :: Tasty.TestTree
+quickCheckTests = 
+  Tasty.testGroup "QuickCheck Tests"
+  $ [ Tasty.testProperty "Test that a peerId is the same after \
+                      \encoding and decoding"
+        $ prop_encodeEquals
+
+    , Tasty.testProperty "Tests that generated PeerId matches PublicKey"
+        $ prop_matchPublicKey
+
+    , Tasty.testProperty "Tests that generated PeerId matches PrivateKey"
+        $ prop_matchPrivateKey ]
+
+
+correctRSAEncoding :: RSAKeyPeerIdPair -> Tasty.TestTree
+correctRSAEncoding kp
+  = Tasty.testCase "Test that PeerIds from RSA match the sample key encodings"
+    $ Tasty.assertEqual "" pid pid2
+    where
+      peerId = base58PeerId kp
+      skString = base64RSAPrivateKey kp
+      Right skBytes = MHB.decode MHB.Base64 $ BSLChar.pack skString
+      Right (Crypto.RSAPriv sk) = Crypto.parseKey $ BSL.toStrict skBytes
+      Right pid = fromB58 $ BSChar.pack peerId
+      pid2 = rsaToPeerId $ Crypto.toPublic sk
+
+
+unitRSATests :: [RSAKeyPeerIdPair] -> Tasty.TestTree
+unitRSATests kp =
+  Tasty.testGroup "Unit Tests" testKeyPairs
+
+  where tests = [correctRSAEncoding]
+        testKeyPairs = liftM2 ($) tests kp
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  file <- BS.readFile "test/RSATestCases.json"
+  let Just rsakp = (Aeson.decodeStrict file :: Maybe [RSAKeyPeerIdPair])
+  let runTests = Tasty.testGroup "All Tests" 
+                   $ [ quickCheckTests
+                     , unitRSATests rsakp ]
+  Tasty.defaultMain runTests
+
